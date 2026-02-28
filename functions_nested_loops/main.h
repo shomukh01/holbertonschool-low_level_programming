0fe209287c59ea690741c48efe505bfc67696437
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-02-28 10:29:59 +0000

    Fix main.h formatting

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,11 +5,3 @@ int _putchar(char c);
 void print_alphabet(void);
 
 #endif
-~                                                                                                                                                                                         
-~                                                                                                                                                                                         
-~                                                                                                                                                                                         
-~                                                                                                                                                                                         
-~                                                                                                                                                                                         
-~                                                                                                                                                                                         
-~                                                                                                                                                                                         
-~            
