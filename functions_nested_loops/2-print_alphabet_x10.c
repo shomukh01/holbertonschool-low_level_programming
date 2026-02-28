49ddb691c800692e31e46554326f2476b7e97f2f
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-02-28 10:57:36 +0000

    Fix Betty indentation

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints alphabet 10 times
- */
+	* print_alphabet_x10 - prints alphabet 10 times
+	*/
 void print_alphabet_x10(void)
 {
- int i;
- char c;
+	int i;
+	char c;
 
- for (i = 0; i < 10; i++)
- {
-  c = 'a';
-  while (c <= 'z')
-  {
-   _putchar(c);
-   c++;
-  }
-  _putchar('\n');
- }
+	for (i = 0; i < 10; i++)
+	{
+	c = 'a';
+	while (c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
+	_putchar('\n');
+	}
 }
