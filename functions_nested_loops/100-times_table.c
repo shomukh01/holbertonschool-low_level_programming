063d1b1fb67f6f701a4f6ee2168c94847e4a13c5
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-02-28 20:02:43 +0000

    Final fix 100-times_table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
 /**
-	* print_number - prints a number with correct formatting
+	* print_number - prints a number with correct spacing
 	* @n: number to print
 	*/
 void print_number(int n)
 {
-	if (n >= 100)
+	if (n < 10)
 	{
-	_putchar((n / 100) + '0');
-	_putchar(((n / 10) % 10) + '0');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(n + '0');
+	}
+	else if (n < 100)
+	{
+	_putchar(' ');
+	_putchar(n / 10 + '0');
+	_putchar(n % 10 + '0');
 	}
 	else
 	{
-	_putchar(' ');
-	if (n >= 10)
-	_putchar((n / 10) + '0');
-	else
-	_putchar(' ');
+	_putchar(n / 100 + '0');
+	_putchar((n / 10) % 10 + '0');
+	_putchar(n % 10 + '0');
 	}
-	_putchar((n % 10) + '0');
 }
 
 /**
@@ -38,10 +42,11 @@ void print_times_table(int n)
 	for (j = 0; j <= n; j++)
 	{
 	if (j == 0)
-	print_number(i * j);
+	_putchar('0');
 	else
 	{
 	_putchar(',');
+	_putchar(' ');
 	print_number(i * j);
 	}
 	}
