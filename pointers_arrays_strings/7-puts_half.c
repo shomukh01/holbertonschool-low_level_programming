fdee127642e56e479ddd42e0a79d464076d0a25b
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-03-05 19:38:56 +0000

    tasks 6 7 8 9

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+	* puts_half - prints second half of a string
+	* @str: string
+	*/
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len + 1) / 2;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
