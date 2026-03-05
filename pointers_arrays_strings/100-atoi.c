e07f5a63bfb5f1401f032b9a6f8b2b4bf6b88abf
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-03-05 19:49:06 +0000

    task 10 atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+	* _atoi - convert string to integer
+	* @s: string
+	* Return: integer
+	*/
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int num = 0;
+	int started = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			num = num * 10 + (s[i] - '0');
+		}
+
+		else if (started)
+			break;
+
+		i++;
+	}
+
+	return (num * sign);
+}
