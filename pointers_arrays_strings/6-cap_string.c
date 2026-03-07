6b2708e922c457cbd3c74bcaf268e2cdad45ff48
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-03-07 20:16:00 +0000

    Fix cap_string correctly

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
-	* cap_string - capitalizes all words of a string
-	* @str: string
-	*
-	* Return: pointer to the string
-	*/
+* cap_string - capitalizes all words of a string
+* @str: string
+*
+* Return: pointer to the string
+*/
 char *cap_string(char *str)
 {
-	int i = 0;
-	int cap = 1;
+int i = 0;
+int cap = 1;
 
-	while (str[i])
-	{
-	if (str[i] == ' '  str[i] == '\t'  str[i] == '\n' ||
-	str[i] == ','  str[i] == ';'  str[i] == '.' ||
-	str[i] == '!'  str[i] == '?'  str[i] == '"' ||
-	str[i] == '('  str[i] == ')'  str[i] == '{' ||
-	str[i] == '}')
-	{
-	cap = 1;
-	}
-	else if (cap && str[i] >= 'a' && str[i] <= 'z')
-	{
-	str[i] -= 32;
-	cap = 0;
-	}
-	else
-	{
-	cap = 0;
-	}
-
-	i++;
-	}
-
-	return (str);
+while (str[i])
+{
+if (str[i] == ' '  str[i] == '\t'  str[i] == '\n' ||
+str[i] == ','  str[i] == ';'  str[i] == '.' ||
+str[i] == '!'  str[i] == '?'  str[i] == '"' ||
+str[i] == '('  str[i] == ')'  str[i] == '{' ||
+str[i] == '}')
+{
+cap = 1;
+}
+else if (cap && str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+cap = 0;
+}
+else
+{
+cap = 0;
+}
+i++;
+}
+return (str);
 }
