4da3f1d6382dbcf1aec4cff166fa600a5f7ef34a
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-03-07 19:15:41 +0000

    Add cap_string and leet

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+	* cap_string - capitalizes all words of a string
+	* @str: string
+	*
+	* Return: pointer to string
+	*/
+char *cap_string(char *str)
+{
+	int i = 0;
+	int capitalize = 1;
+
+	while (str[i])
+	{
+	if (str[i] == ' '  str[i] == '\t'  str[i] == '\n' ||
+	str[i] == ','  str[i] == ';'  str[i] == '.' ||
+	str[i] == '!'  str[i] == '?'  str[i] == '"' ||
+	str[i] == '('  str[i] == ')'  str[i] == '{' ||
+	str[i] == '}')
+	capitalize = 1;
+	else if (capitalize && str[i] >= 'a' && str[i] <= 'z')
+	{
+	str[i] -= 32;
+	capitalize = 0;
+	}
+	else
+	capitalize = 0;
+
+	i++;
+	}
+
+	return (str);
+}
---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+	* leet - encodes a string into 1337
+	* @str: string
+	*
+	* Return: pointer to string
+	*/
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; str[i]; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (str[i] == letters[j])
+	str[i] = numbers[j];
+	}
+	}
+
+	return (str);
+}
