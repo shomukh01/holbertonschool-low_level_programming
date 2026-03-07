1ad94aa8cef0d62bb94a1872de1b9b068ffa16a3
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-03-07 13:18:04 +0000

    Fix Betty indentation manually

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings
- * @dest: destination buffer
- * @src: source buffer
- * @n: maximum number of bytes to use from src
- *
- * Return: pointer to dest
- */
+* _strncat - concatenates two strings
+* @dest: destination buffer
+* @src: source buffer
+* @n: maximum number of bytes from src
+*
+* Return: pointer to dest
+*/
 char *_strncat(char *dest, char *src, int n)
 {
- int i = 0;
- int j = 0;
+int i = 0;
+int j = 0;
 
- while (dest[i] != '\0')
- {
-  i++;
- }
+while (dest[i] != '\0')
+{
+i++;
+}
 
- while (src[j] != '\0' && j < n)
- {
-  dest[i] = src[j];
-  i++;
-  j++;
- }
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
 
- dest[i] = '\0';
+dest[i] = '\0';
 
- return (dest);
+return (dest);
 }
