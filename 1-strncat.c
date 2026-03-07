ea98fb97b8b92a9c7e5df4b182d0d98bcd6b4ea7
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-03-07 12:38:45 +0000

    Add 1-strncat

---
+++ 1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination buffer
+ * @src: source buffer
+ * @n: maximum number of bytes to use from src
+ *
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+ int i = 0;
+ int j = 0;
+
+ while (dest[i] != '\0')
+ {
+  i++;
+ }
+
+ while (src[j] != '\0' && j < n)
+ {
+  dest[i] = src[j];
+  i++;
+  j++;
+ }
+
+ dest[i] = '\0';
+
+ return (dest);
+}
