ca59f4de9ca8c4f9551df3af33eab2538310a53e
Shomukh01 <shomukh01@github.com>
2026-03-12 22:58:59 +0000

    fix betty

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
+/**
+ * _memset - fills memory with constant byte
+ * @s: pointer to memory
+ * @b: byte to fill
+ * @n: number of bytes
+ *
+ * Return: pointer to s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-    return (s);
+	return (s);
 }
