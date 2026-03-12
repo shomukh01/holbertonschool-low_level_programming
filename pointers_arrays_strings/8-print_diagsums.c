f59dd91e0541a513b51028a8e1de32af5a8a5aea
Shomukh01 <shomukh01@github.com>
2026-03-12 23:18:56 +0000

    task 7 diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+	* print_diagsums - prints sum of diagonals
+	* @a: matrix
+	* @size: size of matrix
+	*/
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - i - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
