b1e485fc1d10bd922a6f8518576087ea5df88f79
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-03-13 10:18:56 +0000

    task 3 factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial of number
+ * @n: number
+ * Return: factorial or -1
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -4,4 +4,5 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 #endif
