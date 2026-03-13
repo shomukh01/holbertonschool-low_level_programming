56ffad4ee87fddb48ff57adc284000680962c116
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-03-13 10:37:22 +0000

    fix task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,9 +1,26 @@
+#include "main.h"
+
+int helper(int n, int i);
+
 /**
- * helper - finds natural square root
- * @n: number
- * @i: trial number
- * Return: sqrt or -1
- */
+	* _sqrt_recursion - returns natural square root
+	* @n: number
+	* Return: sqrt or -1
+	*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (helper(n, 0));
+}
+
+/**
+	* helper - finds natural square root
+	* @n: number
+	* @i: trial number
+	* Return: sqrt or -1
+	*/
 int helper(int n, int i)
 {
 	if (i * i == n)
