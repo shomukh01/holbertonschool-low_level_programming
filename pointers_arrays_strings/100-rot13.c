8ca13cdf7d3c32d9bcd0337c096c1998da7385bd
root <Shomukh.aldosari@gmail.com~>
2026-03-14 07:19:44 +0000

    rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+	* rot13 - encodes a string using rot13
+	* @s: string
+	* Return: encoded string
+	*/
+char *rot13(char *s)
+{
+	int i = 0;
+	int j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (s[i])
+	{
+		j = 0;
+		while (a[j])
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
