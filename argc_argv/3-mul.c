cf69b89e75dec400708a9556962e84842b5ae1c8
root <Shomukh.aldosari@gmail.com~>
2026-03-14 07:04:00 +0000

    task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+
+	printf("%d\n", n1 * n2);
+
+	return (0);
+}
