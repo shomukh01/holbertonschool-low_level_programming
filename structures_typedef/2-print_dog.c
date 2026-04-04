b1d349a995b8cb3ca2f9e2e4f1088e582ad6479e
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-04-04 11:12:19 +0000

    add print_dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -22,5 +22,5 @@ struct dog
  * @owner: owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 #endif
