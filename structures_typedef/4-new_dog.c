f57e540204ba9159a1337634b37dc31de5b6effe
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-04-06 08:49:06 +0000

    Task 4 new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,55 +1,56 @@
 #include "dog.h"
 #include <stdlib.h>
 
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to new dog, or NULL if fail
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new;
-	int len_name, len_owner;
+    dog_t *dog;
+    int i, len_name = 0, len_owner = 0;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
+    if (name == NULL || owner == NULL)
+        return (NULL);
 
-	new = malloc(sizeof(dog_t));
-	if (new == NULL)
-		return (NULL);
+    for (i = 0; name[i]; i++)
+        len_name++;
 
-	len_name = _strlen(name);
-	len_owner = _strlen(owner);
+    for (i = 0; owner[i]; i++)
+        len_owner++;
 
-	new->name = malloc(len_name + 1);
-	if (new->name == NULL)
-		return (NULL);
+    dog = malloc(sizeof(dog_t));
+    if (dog == NULL)
+        return (NULL);
 
-	new->owner = malloc(len_owner + 1);
-	if (new->owner == NULL)
-		return (NULL);
+    dog->name = malloc(len_name + 1);
+    if (dog->name == NULL)
+    {
+        free(dog);
+        return (NULL);
+    }
 
-	_strcpy(new->name, name);
-	_strcpy(new->owner, owner);
+    dog->owner = malloc(len_owner + 1);
+    if (dog->owner == NULL)
+    {
+        free(dog->name);
+        free(dog);
+        return (NULL);
+    }
 
-	new->age = age;
+    for (i = 0; i < len_name; i++)
+        dog->name[i] = name[i];
+    dog->name[len_name] = '\0';
 
-	return (new);
+    for (i = 0; i < len_owner; i++)
+        dog->owner[i] = owner[i];
+    dog->owner[len_owner] = '\0';
+
+    dog->age = age;
+
+    return (dog);
 }
