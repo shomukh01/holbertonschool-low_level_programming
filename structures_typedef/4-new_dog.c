46bcde81507832a1d1838cb03969524fd199a881
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-04-06 08:59:56 +0000

    Refactor new_dog for Betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,54 +2,76 @@
 #include <stdlib.h>
 
 /**
-	* new_dog - creates a new dog
-	* @name: name of the dog
-	* @age: age of the dog
-	* @owner: owner of the dog
-	*
-	* Return: pointer to new dog, or NULL if fail
-	*/
+ * _strlen - returns length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * copy_string - copies a string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: nothing
+ */
+void copy_string(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to new dog, or NULL if fail
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int i, len_name = 0, len_owner = 0;
+	int len_name, len_owner;
 
 	if (name == NULL || owner == NULL)
-	return (NULL);
+		return (NULL);
 
-	for (i = 0; name[i]; i++)
-	len_name++;
-
-	for (i = 0; owner[i]; i++)
-	len_owner++;
+	len_name = _strlen(name);
+	len_owner = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-	return (NULL);
+		return (NULL);
 
 	dog->name = malloc(len_name + 1);
 	if (dog->name == NULL)
 	{
-	free(dog);
-	return (NULL);
+		free(dog);
+		return (NULL);
 	}
 
 	dog->owner = malloc(len_owner + 1);
 	if (dog->owner == NULL)
 	{
-	free(dog->name);
-	free(dog);
-	return (NULL);
+		free(dog->name);
+		free(dog);
+		return (NULL);
 	}
 
-	for (i = 0; i < len_name; i++)
-	dog->name[i] = name[i];
-	dog->name[len_name] = '\0';
-
-	for (i = 0; i < len_owner; i++)
-	dog->owner[i] = owner[i];
-	dog->owner[len_owner] = '\0';
-
+	copy_string(dog->name, name);
+	copy_string(dog->owner, owner);
 	dog->age = age;
 
 	return (dog);
