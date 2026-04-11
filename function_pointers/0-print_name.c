00d7628286182822ad7873a2433ccd5d383c4407
root <Shomukh.aldosari@gmail.com~>
2026-04-11 10:03:38 +0000

    Fix Betty style

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,14 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using a function pointer
- * @name: the name to print
- * @f: function pointer
- */
+	* print_name - prints a name using a function pointer
+	* @name: the name to print
+	* @f: function pointer
+	*/
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    {
-        f(name);
-    }
+	if (name != NULL && f != NULL)
+	{
+	f(name);
+	}
 }
