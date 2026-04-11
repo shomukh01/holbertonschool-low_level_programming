855a728a646bfc4d1896d35842c8ec5765f3cfb6
root <Shomukh.aldosari@gmail.com~>
2026-04-11 10:01:33 +0000

    Move task 0 files to function_pointers directory

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: the name to print
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name != NULL && f != NULL)
+    {
+        f(name);
+    }
+}
