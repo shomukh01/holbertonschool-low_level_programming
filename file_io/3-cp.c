aed6c27fe0f7f532b2544ece36d98148d6ca9727
shomukh01 <Shomukh.aldosari@gmail.com>
2026-04-26 00:58:33 +0000

    Fix Betty warnings cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,15 +4,12 @@
 #include <stdio.h>
 
 /**
-	* close_file - closes a file descriptor
+	* close_file - closes file descriptor
 	* @fd: file descriptor
 	*/
 void close_file(int fd)
 {
-	int c;
-
-	c = close(fd);
-	if (c == -1)
+	if (close(fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -20,17 +17,48 @@ void close_file(int fd)
 }
 
 /**
-	* main - copies content of one file to another
-	* @argc: argument count
-	* @argv: arguments
+	* copy_file - copies content
+	* @fd_from: source
+	* @fd_to: destination
+	* @file_to: destination name
+	* @file_from: source name
+	*/
+void copy_file(int fd_from, int fd_to, char *file_to, char *file_from)
+{
+	ssize_t r, w;
+	char buffer[1024];
+
+	while ((r = read(fd_from, buffer, 1024)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+		{
+			close_file(fd_from);
+			close_file(fd_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		close_file(fd_from);
+		close_file(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+}
+
+/**
+	* main - cp program
+	* @argc: arg count
+	* @argv: args
 	*
-	* Return: 0 on success
+	* Return: 0
 	*/
 int main(int argc, char **argv)
 {
 	int fd_from, fd_to;
-	ssize_t r, w;
-	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -53,28 +81,11 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
-	while ((r = read(fd_from, buffer, 1024)) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w == -1 || w != r)
-		{
-			close_file(fd_from);
-			close_file(fd_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-
-	if (r == -1)
-	{
-		close_file(fd_from);
-		close_file(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	copy_file(fd_from, fd_to, argv[2], argv[1]);
 
 	close_file(fd_from);
 	close_file(fd_to);
 
 	return (0);
 }
+
