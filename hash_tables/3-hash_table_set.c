a441c0b1ee806d1bb7b0949081c29b4107aebdd5
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-05-02 10:27:38 +0000

    Fix betty and split function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,31 +3,23 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table to add or update the key/value to
- * @key: key to add
- * @value: value associated with the key
+ * update_value - updates value if key exists
+ * @current: current node
+ * @key: key
+ * @value: value
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: 1 if updated, 0 otherwise
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int update_value(hash_node_t *current, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *node, *current;
 	char *value_copy;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	current = ht->array[index];
-
-	while (current != NULL)
+	while (current)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
 			value_copy = strdup(value);
-			if (value_copy == NULL)
+			if (!value_copy)
 				return (0);
 			free(current->value);
 			current->value = value_copy;
@@ -35,14 +27,38 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
+	return (0);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if success, 0 if fail
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key || *key == '\0' || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (update_value(ht->array[index], key, value))
+		return (1);
 
 	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	if (!node)
 		return (0);
 
 	node->key = strdup(key);
 	node->value = strdup(value);
-	if (node->key == NULL || node->value == NULL)
+
+	if (!node->key || !node->value)
 	{
 		free(node->key);
 		free(node->value);
