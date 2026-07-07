4c0df3c95b40eddaffeba9ec64a2386bc0d73ad8
Shomukh Al Dosari <shomukh.aldosari@gmail.com>
2026-07-07 11:08:50 +0200

    Add assembly hello world

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,17 @@
+global main
+
+section .data
+msg db "Hello, World", 10
+len equ $ - msg
+
+section .text
+main:
+mov rax, 1
+mov rdi, 1
+mov rsi, msg
+mov rdx, len
+syscall
+
+mov rax, 60
+xor rdi, rdi
+syscall
